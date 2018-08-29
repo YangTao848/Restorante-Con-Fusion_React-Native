07601cd4b0992335ed963a72ec6be3e8f6130a2e
Manik Sharma <maniksharma98144@gmail.com>
2018-08-30 00:53:49 +0530

    final project

--- shared/baseUrl.js
+++ shared/baseUrl.js
@@ -1,6 +1,1 @@
-export const baseUrl = 'http://192.168.43.105:3001/';
-
-/* To Run json-server,
-command:
-json-server .\db.json --w --d 1900 --p 3001 --H 192.168.43.105
-*/
+export const baseUrl = 'http://[IP address:3001]/';
